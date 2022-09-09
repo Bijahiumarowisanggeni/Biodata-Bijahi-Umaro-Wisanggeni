aa7b49b008ee2a3670b02d79eb199da579c1f55a
Bijahiumarowisanggeni <113006037+Bijahiumarowisanggeni@users.noreply.github.com>
2022-09-09 15:50:59 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama: BIjahi Umaro Wisanggeni" << endl;
+    cout << "NIM:A11.2022.14589" << endl;
+    cout << "Alamat: KEL. MOJOREMBUN KEC. KRADENAN BLORA" << endl;
+    cout << "Fakultas: Ilmu Komputer" << endl;
+    cout << "PRODI: Teknik Informatika" << endl;
+    cout << "TTL: Blora, 25 April 2003" << endl;
+    return 0;
+}
